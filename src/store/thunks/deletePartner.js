7ef2907e65a6dcec2a93b7125586ed641755a5e4
Daya Chettri <Dayasagar073@gmail.com>
2023-04-27 11:53:48 +0530

    add partner update

--- src/store/thunks/deletePartner.js
+++ src/store/thunks/deletePartner.js
@@ -4,7 +4,6 @@ import axios from "axios";
 const deletePartner = createAsyncThunk(
   "partner/delete",
   async ({ token, object }) => {
-    console.log("delete thunk clicked", token, object);
     await axios.delete(
       `https://dev-api.navgurukul.org/apiDocs/partners/${object.id}`,
       {
