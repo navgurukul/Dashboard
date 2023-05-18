073bfda979d068318783eab8ceb275a4863ff4da
psd9988 <psd9988@gmail.com>
2023-05-18 11:30:38 +0530

    table pagination

--- src/components/PartnersList/PartnersTable.jsx
+++ src/components/PartnersList/PartnersTable.jsx
@@ -28,10 +28,67 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   backgroundColor: "white",
-  "& : hover": {
+  "&:hover": {
     backgroundColor: "white",
   },
 }));
+
+// Define the colors for status
+const circleColors = {
+  Active: "#48A145",
+  Inactive: "#FFCC00",
+  "Newly Onboarded": "#2196F3",
+  Archived: "#F44336",
+};
+
+// Styled component for the color of status
+const Circle = styled("div")(({ color }) => ({
+  width: "8px",
+  height: "8px",
+  borderRadius: "50%",
+  backgroundColor: color,
+  display: "inline-block",
+  marginRight: "5px",
+ 
+}));
+
+const typographyStyles1 = {
+  color: "black",
+  textDecoration: "none",
+  width: "283.5px",
+  height: "21px",
+  fontSize: "14px",
+  fontWeight: "400",
+  lineHeight: "150%",
+};
+
+const typographyStyles2 = {
+  color: "rgba(109, 109, 109, 1)",
+  textDecoration: "none",
+  width: "283.5px",
+  height: "21px",
+  fontSize: "14px",
+  fontWeight: "400",
+  lineHeight: "150%",
+};
+
+const typographyStyles3 = {
+  color: "black",
+  textDecoration: "none",
+  width: "16px",
+  height: "21px",
+  fontSize: "14px",
+  fontWeight: "400",
+  lineHeight: "150%",
+};
+
+const typographyStyles4 = {
+  color: "black",
+  textDecoration: "none",
+  fontSize: "14px",
+ 
+  
+};
 
 function PartnersTable({ data }) {
   const [removePartner, results] = useRemovePartnerMutation();
@@ -40,11 +97,11 @@ function PartnersTable({ data }) {
   const [updateData, setUpdateData] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const partnersPerPage = Math.ceil(data.length/3);
+  const partnersPerPage = 10;
   const indexOfLastPartner = currentPage * partnersPerPage;
   const indexOfFirstPartner = indexOfLastPartner - partnersPerPage;
   const currentPartners = data.slice(indexOfFirstPartner, indexOfLastPartner);
-  const pageNumbers = 3;
+  const [pageNumbers, setPageNumbers] = useState(Math.ceil(data.length));
   // const pageNumbers = Math.ceil(data.length / partnersPerPage);
 
   const handlePageChange = (event, value) => {
@@ -74,7 +131,7 @@ function PartnersTable({ data }) {
     <StyledTableCell>
       <Button
         onClick={() => handleEditClick(rowData)}
-        sx={{ color: "#BDBDBD", "&:hover": { color: "info.main" } }}
+        sx={{ height:"18px", width:"18px", color: "#BDBDBD", "&:hover": { color: "info.main" } }}
       >
         <EditIcon>Edit</EditIcon>
       </Button>
@@ -82,11 +139,11 @@ function PartnersTable({ data }) {
         onClick={() => {
           handleEmailClick(rowData);
         }}
-        sx={{ color: "#BDBDBD", "&:hover": { color: "primary.main" } }}
+        sx={{ height:"18px", width:"18px", color: "#BDBDBD", "&:hover": { color: "primary.main" } }}
       >
         <EmailIcon>Email</EmailIcon>
       </Button>
-      <Button sx={{ color: "#BDBDBD", "&:hover": { color: "error.main" } }}>
+      <Button sx={{ height:"18px", width:"18px", color: "#BDBDBD", "&:hover": { color: "error.main" } }}>
         <DeleteIcon onClick={() => handleDeleteClick(rowData)}>
           Delete
         </DeleteIcon>
@@ -95,23 +152,31 @@ function PartnersTable({ data }) {
   );
 
   const renderedData = currentPartners.map((row) => (
-    <StyledTableRow key={row.id} sx={{ border: "none" }}>
+    <StyledTableRow key={row.id} sx={{ border: "none"}}>
       <StyledTableCell>
         <Link to={`partnerspace/${row.id}`} style={{ textDecoration: "none" }}>
-          <Typography sx={{ color: "black", textDecoration: "none" }}>
-            {row.name}
-          </Typography>
+          <Typography sx={typographyStyles1}>{row.name}</Typography>
         </Link>
       </StyledTableCell>
       <StyledTableCell>
-        <Typography>{row.point_of_contact_name || "---"}</Typography>
+        <Typography sx={{ fontSize: "14px" }}>
+          {row.point_of_contact_name || "---"}
+        </Typography>
       </StyledTableCell>
       <StyledTableCell>
-        <Typography>{row.user || "---"}</Typography>
+        <Typography sx={typographyStyles3}>{row.user || "---"}</Typography>
       </StyledTableCell>
-      <StyledTableCell>
-        <Typography>{row.status || "---"}</Typography>
+
+      <StyledTableCell sx={{display: "flex", alignItems:"center"}}>
+        {row.status && (
+          <>
+            <Circle color={circleColors[row.status]} />
+            <Typography sx={typographyStyles4}>{row.status}</Typography>
+          </>
+        )}
+        {!row.status && <Typography sx={typographyStyles4}>---</Typography>}
       </StyledTableCell>
+
       <Actions rowData={row} />
     </StyledTableRow>
   ));
@@ -126,7 +191,7 @@ function PartnersTable({ data }) {
         />
       )}
       <Box>
-        <TableContainer>
+        <TableContainer sx={{overflow:"hidden"}}>
           <Table>
             <TableHead>
               <StyledTableRow sx={{ borderBottom: 2, borderColor: "#BDBDBD" }}>
@@ -154,16 +219,19 @@ function PartnersTable({ data }) {
           display="flex"
           sx={{ p: 2, gap: 22, color: "rgba(109, 109, 109, 1)" }}
         >
-          <Typography>
-            Showing {currentPage * partnersPerPage - (partnersPerPage - 1)} - {currentPage * partnersPerPage} of {data.length}{" "}
-            partners{" "}
+          <Typography sx={typographyStyles2}>
+            Showing {currentPage * partnersPerPage - (partnersPerPage - 1)} -{" "}
+            {currentPage * partnersPerPage} of {data.length} partners{" "}
           </Typography>
 
           <Pagination
-            count={pageNumbers}
+            count={Math.ceil(data.length/10)}
             color="primary"
             page={currentPage}
             onChange={handlePageChange}
+            // defaultPage={3}
+            // siblingCount={0}
+            // boundaryCount={1}
           />
         </Box>
       </Box>
