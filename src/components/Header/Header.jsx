385cc19fdad10ee16b5e71b53d0f01ad5c41296a
psd9988 <psd9988@gmail.com>
2023-08-11 18:32:00 +0530

    Loaders added plus logout issue fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Box, Menu, MenuItem } from "@mui/material";
 import { Link } from "react-router-dom";
 import headerLogo from "./asset/logo.png";
-// import studentProfilePhoto from "./asset/unnamed.png";
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
+  const userLocalData = JSON.parse(localStorage.getItem('userData'));
 
-  const handleProfileClick = () => {
-    setIsMenuOpen(true);
+  const handleProfileClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
-  const userLocalData=JSON.parse(localStorage.getItem('userData'))
 
   const handleMenuClose = () => {
-    setIsMenuOpen(false);
+    setAnchorEl(null);
   };
 
   const handleLogOut = () => {
-    setIsMenuOpen(false);
+    setAnchorEl(null);
     localStorage.clear();
     navigate("/");
   };
   
-  // Close the menu when clicking outside
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        handleMenuClose();
-      }
-    };
- 
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
   return (
     <Box
       sx={{
@@ -50,7 +34,6 @@ const Header = () => {
         top: "0",
         width: "100%",
         backgroundColor: "white",
-        zIndex: "9000"
       }}
     >
       {/* Link to homepage */}
@@ -67,9 +50,8 @@ const Header = () => {
           onClick={handleProfileClick}
         />
         <Menu
-          anchorReference="anchorPosition"
-          anchorPosition={{ top: 70, left: 1500 }} // Adjust the position as needed
-          open={isMenuOpen}
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
           onClose={handleMenuClose}
         >
           {/* Link to profile page */}
