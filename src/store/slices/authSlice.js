bb1809d39fe51bcaa7d628b03c70ac713ff66f36
amansharmma <aman18@navgurukul.org>
2023-08-19 15:46:36 +0530

    Fixed the Calendar event Error.

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const tokenId = JSON.parse(localStorage.getItem("AUTH"));
+
 const authSlice = createSlice({
   name: "auth",
   initialState: {
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM0NTAxIiwiZW1haWwiOiJhYWRhcnNoMjFAbmF2Z3VydWt1bC5vcmciLCJpYXQiOjE2ODk1NjkyMDQsImV4cCI6MTcyMTEyNjgwNH0.Dx-elw94WqsIAI3b1LT4Yxlq13SE8zhaaEovInFEuqc"
+    token: tokenId.token,
   },
   reducers: {},
   extraReducers(builder) {},
